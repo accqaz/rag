b6c23eb42f291b6abcbb51017704a9d4de66318d
accqaz <3350391274@qq.com>
2024-06-26 13:34:24 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -5,9 +5,10 @@ mkdir bin
 
 # install ollama
 wget "https://modelscope.cn/api/v1/models/issaccv/OllamaDeploy/repo?Revision=master&FilePath=ollama-linux-amd64" -O bin/ollama
+chmod +x bin/ollama
 
 # run ollama in the background
-bin/ollama serve &
+OLLAMA_FLASH_ATTENTION=1 bin/ollama serve &
 
 # download model 
 wget "https://www.modelscope.cn/api/v1/models/qwen/Qwen2-7B-Instruct-GGUF/repo?Revision=master&FilePath=qwen2-7b-instruct-q8_0.gguf" -O qwen-2-7b-instruct.gguf
